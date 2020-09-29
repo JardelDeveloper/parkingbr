2dd8a37074eb4760af337f52cc051674baa7420a
Jardel <developerprojectfullstack@gmail.com>
2020-09-29 15:41:42 -0300

    update the settings

--- gerenciador_tarefas/settings.py
+++ gerenciador_tarefas/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,10 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, '/static')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'gerenciador_tarefas/static')
+]
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
